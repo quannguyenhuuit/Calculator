257f8d7c0530f1ad91a3f5368854a8d4bc1914f8
quanmacos <quanmacos@quanmacoss-Mac.local>
2017-04-18 19:07:10 +0700

    calculator_final

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,9 +14,54 @@ class ViewController: UIViewController {
     var previousNumber: Double = 0;
     var performingMath = false
     var operation = 0;
+    var afterEqual = false
     
     @IBOutlet weak var label: UILabel!
     
+    //Xu ly button tinh phan tram
+    @IBAction func tinhPhanTram(_ sender: UIButton) {
+        var percent:Double = 0;
+        percent = Double(label.text!)!
+        percent = percent/100
+        label.text = String(percent)
+    }
+
+    //Xoa so 0 cua kieu Double
+    func xoaSoKhong(temp: Double) -> String{
+        let tempVar = String(format: "%g", temp)
+        return tempVar
+    }
+    
+    //Xu ly button nhap Dau Thap Phan (.)
+    @IBAction func nhapDauCham(_ sender: UIButton) {
+        var textResult:String = ""
+        textResult = label.text!
+        let countDos = textResult.characters.filter { $0 == "." }.count
+        if afterEqual == true || label.text == "" {
+            label.text = "0."
+        }
+        else if label.text != "" && countDos == 0 {
+            label.text = label.text! + "."
+        }
+    }
+    
+    //Xu ly button nhap so am (-)
+    @IBAction func nhapSoAm(_ sender: UIButton) {
+        if(Double(label.text!)! == 0)
+        {
+            label.text = "0"
+        }
+        else if(Double(label.text!)! > 0)
+            {
+                label.text = "-" + String(xoaSoKhong(temp: Double(label.text!)!))
+            }
+            else
+            {
+                label.text = String(xoaSoKhong(temp: (-1) * Double(label.text!)!))
+            }
+        }
+    
+    // Nhap cac so tu 0 den 9
     @IBAction func numbers(_ sender: UIButton)
     {
         if performingMath == true
@@ -32,52 +77,57 @@ class ViewController: UIViewController {
         }
     }
     
+    // Xu ly cac button tinh toan + - x /
     @IBAction func buttons(_ sender: UIButton)
     {
         if label.text != "" && sender.tag != 11 && sender.tag != 16
         {
             previousNumber = Double(label.text!)!
-            if sender.tag == 12 // Divide
+            if sender.tag == 12 // Chia
             {
                 label.text = "/";
             }
-            else if sender.tag == 13 // Multiphy
+            else if sender.tag == 13 // Nhan
             {
                 label.text = "x";
             }
-            else if sender.tag == 14 // Minus
+            else if sender.tag == 14 // Tru
             {
                 label.text = "-";
             }
-            else if sender.tag == 15 // Plus
+            else if sender.tag == 15 // Cong
             {
                 label.text = "+";
             }
             operation = sender.tag
             performingMath = true;
         }
+        
+        //Thuc hien phep tinh va xuat ket qua
         else if sender.tag == 16
         {
             if operation == 12
             {
-                label.text = String(previousNumber / numberOnScreen)
+                label.text = String(xoaSoKhong(temp: previousNumber / numberOnScreen))
 
             }
             else if operation == 13
             {
-                label.text = String(previousNumber * numberOnScreen)
+                label.text = String(xoaSoKhong(temp: previousNumber * numberOnScreen))
 
             }
             else if operation == 14
             {
-                label.text = String(previousNumber - numberOnScreen)
+                label.text = String(xoaSoKhong(temp: previousNumber - numberOnScreen))
 
             }
             else if operation == 15
             {
-                label.text = String(previousNumber + numberOnScreen)
+                label.text = String(xoaSoKhong(temp: previousNumber + numberOnScreen))
             }
         }
+        
+        // Button C
         else if sender.tag == 11
         {
             label.text = ""
